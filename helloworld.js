eeb21589f25fac7b1e7c3bf597a2f21b67b6c1d6
Nicholle Romero <Nicholleromero@gmail.com>
2016-03-20 16:47:24 -0700

    new edits

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,7 @@
 
 var username = prompt("Welcome! Who is visiting this page?");
 alert("Hello " + username + "! " + "I hope you enjoy Abby's page!");
-var guessCorrect = 0;
+//var guessCorrect = 0;
 
 var sistersName = "Maggie";
 var guessOne = prompt("What is Abby's sister's name?");
@@ -10,18 +10,20 @@ var nameCorrect = (guessOne.toLowerCase() == sistersName.toLowerCase());
 
 function nameYes(nameCorrect) {
   if(nameCorrect){
-    message = "Correct! Abby's sister's name is Maggie!";
-    document.getElementById("oneR").innerHTML=("X");
-    guessCorrect += 1;
+  //  message = "Correct! Abby's sister's name is Maggie!";
+    document.getElementById("oneR").innerHTML=guessOne+"<img src=https://s-media-cache-ak0.pinimg.com/736x/ea/3d/cf/ea3dcff8ed8e8939d98c96b81f747623.jpg width=30>";
+    document.getElementById("oneR").style.color = "green";
+//    guessCorrect += 1;
   } else {
-    message = "That is not correct.";
-    document.getElementById("oneW").innerHTML=("X");
+  //  message = "That is not correct.";
+    document.getElementById("oneW").innerHTML=guessOne+"<img src=http://www.clipartbest.com/cliparts/ncX/x9d/ncXx9dBcB.png width=30>";
+    document.getElementById("oneW").style.color = "red";
   }
 }
 nameYes(nameCorrect);
 
 console.log("Response to question about sister's name acknowledged.")
-alert(message);
+//alert(message);
 
 var livedIn = "Salem";
 var guessTwo = prompt("What city was Abby raised in?");
@@ -30,18 +32,20 @@ var cityCorrect = (livedIn.toLowerCase() == guessTwo.toLowerCase());
 
 function cityYes(cityCorrect) {
 if (cityCorrect){
-  message = "Correct! She was raised in Salem!";
-  document.getElementById("twoR").innerHTML=("X");
-  guessCorrect += 1;
+//  message = "Correct! She was raised in Salem!";
+    document.getElementById("twoR").innerHTML=guessTwo+"<img src=https://s-media-cache-ak0.pinimg.com/736x/ea/3d/cf/ea3dcff8ed8e8939d98c96b81f747623.jpg width=30>";
+    document.getElementById("twoR").style.color = "green";
+//  guessCorrect += 1;
 } else {
-  message = "Sorry. That is incorrect.";
-  document.getElementById("twoW").innerHTML=("X");
+//  message = "Sorry. That is incorrect.";
+    document.getElementById("twoW").innerHTML=guessTwo+"<img src=http://www.clipartbest.com/cliparts/ncX/x9d/ncXx9dBcB.png width=30>";
+    document.getElementById("twoW").style.color = "red";
   }
 }
 cityYes(cityCorrect)
 
 console.log("Response to city she lived in question.");
-alert(message);
+//alert(message);
 
 var haveDog = "Yes";
 var guessThree = prompt("Does Abby have a dog?");
@@ -50,16 +54,19 @@ var dogCorrect = (haveDog.toLowerCase() == guessThree.toLowerCase());
 
 function dogYes(dogCorrect) {
   if(dogCorrect){
-  message = "You're correct! Abby does have a dog!";
-  document.getElementById("threeR").innerHTML=("X");
-  guessCorrect += 1;
+//  message = "You're correct! Abby does have a dog!";
+  document.getElementById("threeR").innerHTML=guessThree+"<img src=https://s-media-cache-ak0.pinimg.com/736x/ea/3d/cf/ea3dcff8ed8e8939d98c96b81f747623.jpg width=30>";
+  document.getElementById("threeR").style.color = "green";
+//  guessCorrect += 1;
 } else {
-  message = "Sorry. That is incorrect.";
-  document.getElementById("threeW").innerHTML=("X");
+//  message = "Sorry. That is incorrect.";
+  document.getElementById("threeW").innerHTML=guessThree+"<img src=http://www.clipartbest.com/cliparts/ncX/x9d/ncXx9dBcB.png width=30>";
+  document.getElementById("threeW").style.color = "red";
   }
 }
 dogYes(dogCorrect);
 
+/*
 console.log("Response to whether or not Abby has a dog.");
 alert(message);
 
@@ -87,3 +94,6 @@ if (guessCorrect == 4){
 } else {
   alert("You did not pass.");
 }
+
+
+*/
